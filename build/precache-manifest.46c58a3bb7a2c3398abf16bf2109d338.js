3fc58eac75b479b4638791d3716da5d662ca36b2
unknown <kuzma.boh@gmail.com>
2020-03-07 10:52:35 +0100

    optimized build

---
+++ build/precache-manifest.46c58a3bb7a2c3398abf16bf2109d338.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4308da97d86ed81fb2b90559c3e1682b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "408a87ac1b8289b926c8",
+    "url": "/static/css/main.2c348075.chunk.css"
+  },
+  {
+    "revision": "83db392d7ad0c19fec46",
+    "url": "/static/js/2.86c60abf.chunk.js"
+  },
+  {
+    "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
+    "url": "/static/js/2.86c60abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "408a87ac1b8289b926c8",
+    "url": "/static/js/main.400b41ba.chunk.js"
+  },
+  {
+    "revision": "757981370a9ff71f2863",
+    "url": "/static/js/runtime-main.a5fdb4cb.js"
+  },
+  {
+    "revision": "35ef17537983f7815456dc22751e6e7c",
+    "url": "/static/media/Budget.35ef1753.svg"
+  },
+  {
+    "revision": "b4b57f443cb45be71d55eb9e4f4d8504",
+    "url": "/static/media/Exp.b4b57f44.svg"
+  },
+  {
+    "revision": "7b10ab226fdd0bce78f18b004df89c6a",
+    "url": "/static/media/Inc.7b10ab22.svg"
+  },
+  {
+    "revision": "f8dc4af70f3af152561111d356579e95",
+    "url": "/static/media/add.f8dc4af7.svg"
+  },
+  {
+    "revision": "c367e4cc39317d80d7e9716fea6ad8c5",
+    "url": "/static/media/cash.c367e4cc.png"
+  },
+  {
+    "revision": "2446d46ce9180fab1c14e82180aec871",
+    "url": "/static/media/delete.2446d46c.svg"
+  }
+]);
